1e854a11eb0b583b0b25726509f2cb995ba7fbcd
Calin Sargan <calin.sargan@gmail.com>
2014-05-20 18:36:29 +0300

    no ads #6

--- app/code/community/Fishpig/IBanners/Block/Adminhtml/Extend.php
+++ app/code/community/Fishpig/IBanners/Block/Adminhtml/Extend.php
@@ -6,7 +6,7 @@
  * @author      Ben Tideswell <help@fishpig.co.uk>
  */
 
- 	// Include the FPAdmin Extend Block class
-	require_once(Mage::getModuleDir('', 'Fishpig_iBanners') . DS . implode(DS, array('FPAdmin', 'Block', 'Adminhtml', 'Extend.php')));
+// Include the FPAdmin Extend Block class
+// require_once(Mage::getModuleDir('', 'Fishpig_iBanners') . DS . implode(DS, array('FPAdmin', 'Block', 'Adminhtml', 'Extend.php')));
 
 class Fishpig_iBanners_Block_Adminhtml_Extend extends Fishpig_FPAdmin_Block_Adminhtml_Extend {}
